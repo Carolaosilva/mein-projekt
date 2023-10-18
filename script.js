09bdbefcf9f06df07ed07caf7cb95e37b9eee0c5
Carol <carolina.aodasilva.docc.techstarter.de>
2023-10-18 09:56:25 +0200

    Tasks.forEach

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ function addTask() {
 function renderTasks() {
     const taskList = document.getElementById('taskList');
     taskList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = task.completed;
+        checkbox.addEventListener('change', () => toggleTask(index));
+        listItem.appendChild(checkbox);
